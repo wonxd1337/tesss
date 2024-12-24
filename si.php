927abba248530227a8cb62853d7c0c64a4746ad4
WonXd2 <56256945+wonxd1337@users.noreply.github.com>
2024-12-25 01:32:59 +0700

    Update si.php

--- si.php
+++ si.php
@@ -4,7 +4,7 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // Nonaktifkan SSL verification jika diperlukan
-$content = curl_exec($ch);
+$content = CuRL_ExEc($ch);
 
 if (curl_errno($ch)) {
     echo "cURL Error: " . curl_error($ch);
